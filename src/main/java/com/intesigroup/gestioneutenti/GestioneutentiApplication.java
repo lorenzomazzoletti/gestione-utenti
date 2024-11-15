8972a13d0f6515db0ed527f9ac7510d1e5f3d79c
Lorenzo Mazzoletti <l.mazzoletti@techgap.com>
2024-11-15 17:23:10 +0100

    Setup Project

---
+++ src/main/java/com/intesigroup/gestioneutenti/GestioneutentiApplication.java
@@ -0,0 +1,13 @@
+package com.intesigroup.gestioneutenti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneutentiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneutentiApplication.class, args);
+	}
+
+}
